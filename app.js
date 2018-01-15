3e78e00a38fbdc386bfbeb8476d7f8302952591b
Henry Lyford <hlyford@commonsense.org>
2018-01-15 12:21:14 -0800

    change entry point for heroku

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+// -----  Routes
+app.use(express.static('client'));
 
-// Routes
 // Root landing page
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
+  res.sendFile(path.join(__dirname + '/client/index.html'));
 });
+
+// Get one user's data
+app.get('/applicants/:lastName', function(req, res) {
+  var lastName = req.params.lastName;
+  if (req.params.length === 0) {
+    res.send('Please enter with format: applicants/LAST_NAME.json');
+  } else {
+    // See if we have that user    
+    // Get the user's JSON
+    fs.readFile('./users_data/' + lastName + '.json', function(err, data) {
+      console.log(data);
+      res.send(JSON.stringify(data));
+    });    
+  }
+  
+});
+
+// Receives finished application data
+app.post('/responses', function(req, res) {
+  var userObject = req.body.userObject;
+  var lastName = userObject.userInfo.last;  
+  // Store the user data somewhere using last name as identifer
+  fs.writeFile('./users_data/' + lastName + '.json', JSON.stringify(userObject), function() {
+    // Response
+    res.send('cool');
+  });  
+});
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- client/script.js
+++ client/script.js
@@ -5,6 +5,13 @@ $( document ).ready(function() {
     validateForm();
   });
 
+  // Store the user's name and email
+  var userInfo = {
+    first: 'testie',
+    last: 'mcTestFace',
+    email: 'testCity@gmail.com'
+  };
+
   function validateForm() {    
     // Check if the form is complete
     if ( $('form input#first').val() === '' ||
@@ -14,42 +21,18 @@ $( document ).ready(function() {
       alert('You must enter all the fields and accept the terms.')
     } else {
       // If form is complete, open evaluation
-      window.open('./eval.html', '_blank',);
+      userInfo['first'] = $('form input#first').val();
+      userInfo['last'] = $('form input#last').val();
+      userInfo['email']= $('form input#email').val()
+      window.open('./eval.html?q=0', '_blank',);      
     }
   }
 
   // ----- Logic for eval form -----
-  var currentQuestion = 1;
+  var currentQuestion = 0;
   var countElement = $('.count');
   var promptElement = $('.prompt');
   var responseElement = $('.response');
-  
-  // Listen for submit button click
-  $('.submitAnswer').click(function() {
-
-    // if the field is filled out, go on, if not, alert user    
-    var responseText = $(responseElement).val();    
-    if (responseText === '') {
-      alert('Please answer in the space below.');
-    } else {
-      // Store the response
-      questions[currentQuestion - 1].response = responseText; 
-      // Move to next question     
-      currentQuestion++;
-      // Assign the new values in the template
-      var newPrompt = questions[currentQuestion].prompt;
-      var timerValue = questions[currentQuestion].time_allowed;
-      // Append to the DOM
-      $(countElement).html(currentQuestion);
-      $(promptElement).html(newPrompt);
-      // Clear the text area
-      $(responseElement.val(''));
-      // Start the timer
-
-    }    
-  });
-
-
   // Object with question information
   var questions = [
     {
@@ -85,11 +68,97 @@ $( document ).ready(function() {
       response: ''
     },
   ];
+  
+  // Listen for submit button click
+  $('.submitAnswer').click(function() {
+    handleSubmitClick();
+  });  
+
+  function handleSubmitClick() {    
+    // if the field is filled out, go on, if not, alert user    
+    var responseText = $(responseElement).val();    
+    if (responseText === '') {
+      alert('Please answer in the space below.');
+    } else {      
+      // Adjust for index of 0 in questions array      
+      // Store the response
+      questions[currentQuestion].response = responseText; 
+      // Check if it's the last question on the application
+      if (currentQuestion === 4) {
+        finishQuiz();
+      } else {
+        // Move to next question     
+        currentQuestion++;        
+        // Assign the new values in the template
+        var newPrompt = questions[currentQuestion].prompt;
+        var timerValue = questions[currentQuestion].time_allowed;
+        // Append to the DOM
+        $(countElement).html(currentQuestion + 1);
+        $(promptElement).html(newPrompt);
+        // Clear the text area
+        $(responseElement.val(''));
+        // Alter the query string to keep track of question
+
+        // End old timer and start new timer
+        clearInterval(interval);
+        timer();
+      }    
+    }  
+  }
 
 
-  // Helper functions for eval
-  function timer(questionNumber) {
+  // ------- Helper functions for eval
+  var interval;
+  function timer() {    
+    var timeAmount = questions[currentQuestion].time_allowed * 60;
+    var timerElement = $('.timer');
+    // Append to dom intially
+    var formattedTime = fancyTimeFormat(timeAmount);
+    timerElement.text(formattedTime);
+    // Begin interval
+    interval = setInterval(function() {
+      timeAmount--;
+      // Append to dom
+      formattedTime = fancyTimeFormat(timeAmount)
+      $(timerElement).text(formattedTime);
+    }, 1000);    
+  }
 
+  function finishQuiz() {
+    // Send the responses to the server
+    var userObject = {
+      userInfo: userInfo,
+      questions: questions
+    };
+    // Send to server for storage
+    $.ajax({
+      type: 'POST',
+      url: '/responses',
+      data: {userObject: userObject}
+      // success: success      
+    });
+
+    // Show success message to user
+    $('body').html('');
+    $('body').html('<div>Thank you! We\'ll be in touch.');
+  }
+
+  function fancyTimeFormat(time) {   
+    // Hours, minutes and seconds
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = time % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
   }
 
 });
